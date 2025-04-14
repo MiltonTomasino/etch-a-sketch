465673323d50e2aaca6f32186edd68026f587cac
MiltonTomasino <jonathantomasino13@gmail.com>
2025-04-14 10:56:00 -0700

    Create Progressive Darkening Function
    This function allows for a mode where for each pixel passed over, the
    opacity of the pixel increases by 0.1 starting from 0 until it reaches 1.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 let isRandomColorsActive = false;
+let isProgDarkeningActive = false;
+let colorOpacity = 0;
 
 function populateCanvas(size) {
     let canvas = document.querySelector(".canvas");
@@ -17,17 +19,17 @@ function populateCanvas(size) {
         newDiv.style.flex = `0 0 ${pixelSize}px`
         newDiv.addEventListener("mouseover", () => {
             if (!isRandomColorsActive) {
-                newDiv.style.backgroundColor = "black";
+                newDiv.style.backgroundColor = `rgba(0, 0, 0, ${isProgDarkeningActive ? colorOpacity : 1})`;
+                incrementColorOpacity()
             } else {
                 newDiv.style.backgroundColor = randomColor();
+                incrementColorOpacity()
             }
         })
         newDiv.classList.add("pixel");
         canvas.appendChild(newDiv);
     }
 }
-
-populateCanvas(16);
 
 function changeGridSize(size) {
     populateCanvas(size);
@@ -52,14 +54,7 @@ function openPrompt() {
     }
 }
 
-function randomColor() {
-    let R = Math.floor(Math.random() * 256);
-    let G = Math.floor(Math.random() * 256);
-    let B = Math.floor(Math.random() * 256);
-
-    return `rgb(${R}, ${G}, ${B})`;
-}
-
+// Random color functions
 let randomBtn = document.querySelector("#random-btn");
 
 randomBtn.addEventListener("click", () => {
@@ -71,3 +66,38 @@ randomBtn.addEventListener("click", () => {
         randomBtn.textContent = "Random Colors: OFF";
     }
 }) 
+
+function randomColor() {
+    let R = Math.floor(Math.random() * 256);
+    let G = Math.floor(Math.random() * 256);
+    let B = Math.floor(Math.random() * 256);
+
+    return `rgba(${R}, ${G}, ${B}, ${isProgDarkeningActive ? colorOpacity : 1})`;
+}
+
+// Progressive Darkening functions
+let progDarkBtn = document.querySelector("#prog-dark");
+
+progDarkBtn.addEventListener("click", () => {
+    
+    isProgDarkeningActive = !isProgDarkeningActive;
+
+    if (isProgDarkeningActive) {
+        colorOpacity = 0;
+        progDarkBtn.textContent = "Progressive Darkening: ON";
+    } else {
+        colorOpacity = 0;
+        progDarkBtn.textContent = "Progressive Darkening: OFF";
+    }
+});
+
+function incrementColorOpacity() {
+    if (isProgDarkeningActive) {
+        if (colorOpacity < 1) {
+            colorOpacity += 0.1;
+        }
+    }
+}
+
+
+populateCanvas(16);
